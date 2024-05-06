f55d1ce7a1602d787802eff205faec5e64c9983b
SonataGirl <Ino-sonata1988>
2024-05-06 13:06:16 +0300

    Initial Commit

---
+++ SUIProf/SUIProfApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUIProfApp.swift
+//  SUIProf
+//
+//  Created by Sonata Girl on 06.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SUIProfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
